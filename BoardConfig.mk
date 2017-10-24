26f65abe2e9c09db3ecdef81a9e3857a0ed7a4b3
Vojtěch Hejsek <hejsekvojtech@gmail.com>
2017-10-24 19:49:17 +0200

    X5: Fix typo

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -40,7 +40,7 @@ BOARD_KERNEL_CMDLINE += \
 	androidboot.selinux=permissive
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_PAGESIZE := 2048
-BOARD_MKBOOTIMG_ARGS  := --kernel_offset 0x00008000 --ramdisk_offset 0x04000000 --tags_offset 0x0e000000 --board F15
+BOARD_MKBOOTIMG_ARGS  := --kernel_offset 0x00008000 --ramdisk_offset 0x04000000 --tags_offset 0x0e000000 --board R15
 TARGET_PREBUILT_KERNEL := device/doogee/X5/prebuilt/kernel
 
 # TARGET IMAGES
